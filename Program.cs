48d46051c9c80958b1f6068d2bce051d00864e05
nazliozbek <nazlii.ozbek@gmail.com>
2022-04-01 20:03:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trycatchfinally_mantiksalhatalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
